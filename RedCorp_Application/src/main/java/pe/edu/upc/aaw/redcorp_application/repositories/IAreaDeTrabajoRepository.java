c8de7f94791fcb8dc33880ceae8e481b6ca765e8
Mirkito10 <u201724603@upc.edu.pe>
2023-09-24 19:23:20 -0500

    se implementó el query

--- RedCorp_Application/src/main/java/pe/edu/upc/aaw/redcorp_application/repositories/IAreaDeTrabajoRepository.java
+++ RedCorp_Application/src/main/java/pe/edu/upc/aaw/redcorp_application/repositories/IAreaDeTrabajoRepository.java
@@ -1,9 +1,20 @@
 package pe.edu.upc.aaw.redcorp_application.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.aaw.redcorp_application.entities.AreaDeTrabajo;
 
+import java.util.List;
+
 @Repository
 public interface IAreaDeTrabajoRepository extends JpaRepository<AreaDeTrabajo,Integer> {
+    @Query(value = "SELECT aT.nombre as nombre_area_trabajo " +
+            "FROM AreaDeTrabajo aT " +
+            "INNER JOIN MiembroDeArea mA ON a.id = mA.AreaDeTrabajo_id " +
+            "INNER JOIN Usuario u ON mA.Usuario_id = u.id " +
+            "WHERE u.id = :idUsuario", nativeQuery = true)
+    public List<String> userAreasOfWork(@Param("idUsuario") int idUsuario);
+//ver a que areas de trabajo pertenece un usuario
 }
