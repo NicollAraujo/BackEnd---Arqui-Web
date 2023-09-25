b4f5fbdd4656182f630167dc5b1b4b531d27b4e6
XolangeOZ15 <u20201c098@upc.edu.pe>
2023-09-24 19:26:27 -0500

    listo

---
+++ RedCorp_Application/src/main/java/pe/edu/upc/aaw/redcorp_application/dtos/MiembrosTareasDTO.java
@@ -0,0 +1,34 @@
+package pe.edu.upc.aaw.redcorp_application.dtos;
+
+public class MiembrosTareasDTO {
+    public int idTarea;
+    public String nombre;
+    public String descripcion;
+
+    public MiembrosTareasDTO() {
+    }
+
+    public int getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(int idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
